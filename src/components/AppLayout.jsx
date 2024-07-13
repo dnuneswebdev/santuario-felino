625c363317e9308fdf5ee4628573337dd0c3f950
Daniel <dnuneswebdev@gmail.com>
2024-07-13 15:07:05 -0300

    Cria componente Header

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,8 +1,11 @@
 import {Outlet} from "react-router-dom";
+import Header from "./Header";
 
 function AppLayout() {
   return (
     <div className="app-layout">
+      <h1>sidebar</h1>
+      <Header />
       <main>
         <div className="container">
           <Outlet />
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import {BsMoonFill, BsSunFill} from "react-icons/bs";
+import {PiLineVerticalThin} from "react-icons/pi";
+import {MdLogout} from "react-icons/md";
+import {CiMenuBurger} from "react-icons/ci";
+
+function Header() {
+  return (
+    <div className={`bg-white w-full h-14 flex items-center p-4`}>
+      <button className="btn btn-sm rounded-md btn-ghost glass">
+        <CiMenuBurger className="size-5" />
+      </button>
+      <div className="flex justify-end items-center w-full gap-x-2">
+        <div className="avatar flex items-center">
+          <div className="ring-secondary rounded-full ring ring-offset-0 w-8 mr-2">
+            <img src="src/assets/avatar.png" />
+          </div>
+          <p className="font-semibold">Admin</p>
+        </div>
+        <p>
+          <PiLineVerticalThin />
+        </p>
+        <label className="swap swap-rotate">
+          <input type="checkbox" className="theme-controller" />
+          <BsSunFill className="swap-on h-5 w-5" />
+          <BsMoonFill className="swap-off h-5 w-5" />
+        </label>
+        <button className="btn btn-ghost btn-sm rounded-md btn-secondary">
+          <MdLogout className="size-5" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
