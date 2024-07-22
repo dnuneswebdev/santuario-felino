91a7c2e5fa5886bf9f5d1db0b2a175a1ae2ea3e9
Daniel <dnuneswebdev@gmail.com>
2024-07-22 13:21:40 -0300

    Cria página de Funcionários
    Cria nova rota para página de Funcionários

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Login from "./pages/Login";
 import AppLayout from "./components/AppLayout";
 import Error from "./pages/Error";
 import Cats from "./pages/Cats";
+import Employees from "./pages/Employees";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
     errorElement: <Error />,
-    children: [{path: "/cats", element: <Cats />}],
+    children: [
+      {path: "/cats", element: <Cats />},
+      {path: "/employees", element: <Employees />},
+    ],
   },
   {path: "/login", element: <Login />},
 ]);
---
+++ src/data/employees.js
@@ -0,0 +1,37 @@
+export const employees = [
+  {
+    id: 1,
+    name: "João",
+    nationalID: "023.695.784-52",
+    entryDate: "01/09/2021",
+    role: "Veterinário(a)",
+  },
+  {
+    id: 2,
+    name: "Ana",
+    nationalID: "025.395.884-51",
+    entryDate: "05/12/2021",
+    role: "Veterinário(a)",
+  },
+  {
+    id: 3,
+    name: "Fernanda",
+    nationalID: "124.605.774-92",
+    entryDate: "13/04/2020",
+    role: "Jurídico",
+  },
+  {
+    id: 4,
+    name: "Rafaela",
+    nationalID: "627.445.790-30",
+    entryDate: "07/04/2021",
+    role: "Recepção",
+  },
+  {
+    id: 5,
+    name: "Ernesto",
+    nationalID: "783.697.284-12",
+    entryDate: "24/08/2019",
+    role: "Administrativo",
+  },
+];
