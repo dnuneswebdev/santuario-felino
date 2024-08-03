d35beb29e54846073b18bdc99ed52a024e59f496
Daniel <dnuneswebdev@gmail.com>
2024-08-03 09:14:22 -0300

    Cria modal de visualização de Funcionários
    Adiciona novas imagens de Funcionários

--- src/data/employees.js
+++ src/data/employees.js
@@ -3,36 +3,56 @@ export const employees = [
     id: 1,
     name: "João",
     nationalID: "023.695.784-52",
+    rg: "35.767.935-4",
+    age: 45,
     entryDate: "01/09/2021",
+    birthDate: "17/07/1970",
     role: "Veterinário(a)",
+    image: "src/assets/joao.jpg",
   },
   {
     id: 2,
     name: "Ana",
     nationalID: "025.395.884-51",
+    rg: "37.527.705-8",
+    age: 38,
     entryDate: "05/12/2021",
+    birthDate: "17/09/1986",
     role: "Veterinário(a)",
+    image: "src/assets/user.png",
   },
   {
     id: 3,
     name: "Fernanda",
     nationalID: "124.605.774-92",
+    rg: "14.561.736-9",
+    age: 50,
     entryDate: "13/04/2020",
+    birthDate: "02/02/1965",
     role: "Jurídico",
+    image: "src/assets/user.png",
   },
   {
     id: 4,
     name: "Rafaela",
     nationalID: "627.445.790-30",
+    rg: "25.632.981-5",
+    age: 25,
     entryDate: "07/04/2021",
+    birthDate: "23/01/1999",
     role: "Recepção",
+    image: "src/assets/rafaela.jpg",
   },
   {
     id: 5,
     name: "Ernesto",
     nationalID: "783.697.284-12",
+    rg: "95.124.421-9",
+    age: 35,
     entryDate: "24/08/2019",
+    birthDate: "08/06/1989",
     role: "Administrativo",
+    image: "src/assets/user.png",
   },
 ];
 
@@ -43,4 +63,13 @@ export const columns = {
   role: "Cargo",
 };
 
-export const dropdownItems = ["Visualizar", "Editar", "Deletar"];
+export const dropdownItems = [
+  {
+    item: "Visualizar",
+    value: "view",
+  },
+  {
+    item: "Editar",
+    value: "edit",
+  },
+];
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,25 @@
+import {useState} from "react";
 import SectionTitle from "../components/SectionTitle";
 import Table from "../components/Table";
 import {employees, dropdownItems, columns} from "../data/employees";
+import Modal from "../components/Modal";
+import ViewEmployee from "../components/ViewEmployee";
 
 function Employees() {
+  const [employee, setEmployee] = useState({});
+
+  function handleEmployeesOperations(employeeData, mode) {
+    const operationMode = mode !== undefined ? mode : "add";
+    const modal = document.getElementById("modal");
+
+    switch (operationMode) {
+      case "view":
+        setEmployee(employeeData);
+        modal.showModal();
+        break;
+    }
+  }
+
   return (
     <>
       <SectionTitle title="Funcionários" />
@@ -11,7 +28,11 @@ function Employees() {
         data={employees}
         hasActions={true}
         dropdownItems={dropdownItems}
+        handleItemClick={handleEmployeesOperations}
       />
+      <Modal title={employee.name}>
+        <ViewEmployee employee={employee} />
+      </Modal>
     </>
   );
 }
